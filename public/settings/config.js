e9876fda8b286233b5a89690e33b7fd5d60d969a
Kal Stars <haikalramadhan0994@gmail.com>
2025-06-26 06:01:40 +0700

    Update config.js

--- public/settings/config.js
+++ public/settings/config.js
@@ -1,4 +1,4 @@
-global.creator = "@bebas"
-global.token = "TOKEN BOT"
-global.chatid = "USER ID"
-global.watermark = "© bebas"
+global.creator = "@Family95"
+global.token = "8023520188:AAF1rRgnjsKQdXJeqzKQyg04POuL-wCG5N0"
+global.chatid = "5749783178"
+global.watermark = "© Family95"
